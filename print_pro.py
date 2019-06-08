991f6d2f0b925e5f99bd007ba1d4b656ae12a98a
kowshikhari <kowshikhari14@gmail.com>
2019-06-08 18:36:49 +0530

    odd and even

--- print_pro.py
+++ print_pro.py
@@ -2,5 +2,7 @@ num = int(input("Enter the value"))
 print("Odd number")
 for i in range(1,num,2):
     print(i)
-print('HI')
-print("bye")
+print("Even number")
+for i in range(0,num,2):
+    print(i)
+print("Program end")
